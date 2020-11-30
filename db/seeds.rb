48b650ddcf22b2d9fff4d886a5ed3ef7f476bfe5
Ubuntu <ubuntu@ip-172-31-51-252.ec2.internal>
2020-11-30 20:09:07 +0000

    add store

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Store.create(name: 'Ta Quang Buu', info: '1 Dai Co Viet')
+Store.create(name: 'Romma', info: '78 Bui Thi Xuan')
+Store.create(name: 'Ta Quang Buu 2', info: '1 Dai Co Viet')
+Store.create(name: 'Romma 2', info: '78 Bui Thi Xuan')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
