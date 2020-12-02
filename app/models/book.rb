4f1b663dfd970eed6abccddc5336c66525e54b9d
Ubuntu <ubuntu@ip-172-31-29-183.ec2.internal>
2020-12-02 01:12:16 +0000

    Add Destroy Book destroy Genre

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
     has_one_attached :image
     has_many :book_reviews, :dependent => :destroy
+    has_many :book_genres, :dependent => :destroy
     attr_accessor :rate
     attr_accessor :genres
     attr_accessor :reviews
